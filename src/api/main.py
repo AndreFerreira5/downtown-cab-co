30ec514d2d50bd00e09a52002409bf323ee46e74
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-10-28 22:21:23 +0000

    api: added fastapi lifespan with mlflow models

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,9 @@
 import logging
-
+import os
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
+import mlflow.pyfunc
 
 from logging_config import configure_logging
 from routes import (
@@ -11,6 +13,26 @@ from routes import (
 # configure logging globally
 configure_logging()
 logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    mlflow.set_tracking_uri(
+        os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5050")
+    )
+
+    models = []
+    try:
+        models.append(
+            mlflow.pyfunc.load_model(
+                model_uri=f"models:/taxi/Production" # TODO later offload this to a .env file
+            )
+        )
+        logger.info("Model loaded succesfully!")
+    except Exception as e:
+        logger.warning(f"Could not load model at startup: {e}.")
+        logger.warning(f"The model will be lazy loaded.")
+
+
 
 app = FastAPI()
 app.include_router(health.router, prefix="/health", tags=["health"])
