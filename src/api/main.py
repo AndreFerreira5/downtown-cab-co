4c0db656caa6ebbd22d5d3873d75ef21c9f8e880
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-10-28 20:23:20 +0000

    api: logging + health route

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from serving!")
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+
+from logging_config import configure_logging
+from routes import (
+    health
+)
+
+# configure logging globally
+configure_logging()
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+app.include_router(health.router, prefix="/health", tags=["health"])
+
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
