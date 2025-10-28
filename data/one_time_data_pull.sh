5d2562d9dcf5e47b918b9159fbe6a334bad48566
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-10-28 16:57:59 +0000

    new script to pull all the training and testing data and add it to dvc

---
+++ data/one_time_data_pull.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TRAINING_YEARS=("2010" "2011")
+TESTING_YEARS=("2012")
+
+download_year_data() {
+  local year=$1
+  local dest_folder=$2
+  local base_url="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata"
+
+  for month in {01..12}; do
+    url="${base_url}_${year}-${month}.parquet"
+    wget "$url" -P "${dest_folder}" || echo "failed to download ${url}"
+  done
+}
+
+for year in "${TRAINING_YEARS[@]}"; do
+  download_year_data "${year}" "training/"
+  dvc add "training/yellow_tripdata_${year}*"
+done
+
+for year in "${TESTING_YEARS[@]}"; do
+  download_year_data "${year}" "testing/"
+  dvc add "testing/yellow_tripdata_${year}*"
+done
+
+dvc push
