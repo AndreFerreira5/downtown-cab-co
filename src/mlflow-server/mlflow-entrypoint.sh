4f8ae83d6c37c41114001b13e545b765c8c0109b
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-11-02 11:28:40 +0000

    mlflow server: changed port to 3030

--- src/mlflow-server/mlflow-entrypoint.sh
+++ src/mlflow-server/mlflow-entrypoint.sh
@@ -2,7 +2,7 @@
 
 mlflow server \
   --host 0.0.0.0 \
-  --port 5050 \
+  --port 3030 \
   --backend-store-uri sqlite:///mlflow.db \
   --default-artifact-root /mlflow/mlruns \
   --serve-artifacts \
