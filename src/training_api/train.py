826aae2459cbd080127c9c52d50d3f4f2d949e10
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-11-02 11:11:35 +0000

    deploy: fixed wrong server to deploy when deploying training api

--- src/training_api/train.py
+++ src/training_api/train.py
@@ -1,2 +1,11 @@
-﻿def run_training(*args, **kwargs):
+﻿from river import tree
+
+
+model = tree.HoeffdingTreeRegressor(
+    grace_period=50,
+    model_selector_decay=0.3,
+    seed=0
+)
+
+def run_training(*args, **kwargs):
     raise NotImplementedError("Training is not implemented yet.")
