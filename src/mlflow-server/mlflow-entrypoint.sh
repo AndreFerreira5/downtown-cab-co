527b826a8157516a2155c1c9c5f5f0c057efade9
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-11-07 15:11:54 +0000

    mlflow: temporarily removed allowed-hosts because the newest mlflow container doesnt recognize it as an option (?)

--- src/mlflow-server/mlflow-entrypoint.sh
+++ src/mlflow-server/mlflow-entrypoint.sh
@@ -4,6 +4,5 @@ mlflow server \
   --host 0.0.0.0 \
   --port 3030 \
   --backend-store-uri sqlite:///mlflow.db \
-  --default-artifact-root /mlflow/mlruns \
-  --serve-artifacts \
-  --allowed-hosts '*'
+  --default-artifact-root /mlflow/mlruns
+  #--serve-artifacts
