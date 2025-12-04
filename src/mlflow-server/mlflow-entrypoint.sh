154146e2fb64372de9249ed831ed13d79a9299fc
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-12-04 17:18:36 +0000

    mlflow: changed default artifact root and destination so the inference api can fetch the artifacts and load the production model

--- src/mlflow-server/mlflow-entrypoint.sh
+++ src/mlflow-server/mlflow-entrypoint.sh
@@ -4,6 +4,7 @@ mlflow server \
   --host 0.0.0.0 \
   --port 3030 \
   --backend-store-uri sqlite:////mlflow/mlflow.db \
-  --default-artifact-root /mlflow/mlruns \
+  --default-artifact-root mlflow-artifacts:/ \
+  --artifacts-destination /mlflow/mlruns \
   --allowed-hosts "*" \
   --serve-artifacts
