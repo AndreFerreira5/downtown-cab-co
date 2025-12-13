0f0ca06bca1d235a11737bfd4c780cd95199c6ce
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-12-13 16:41:31 +0000

    trigger train

--- src/training_api/main.py
+++ src/training_api/main.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 def train(training_config: TrainingConfig):
     mlflow.set_experiment(f"{training_config.EXP_NAME}_{training_config.COMMIT_SHA}")
 
-    # First get the   best hyperparameters
+    # First get the best hyperparameters
     model_params = run_hyperparameter_tuning(training_config.COMMIT_SHA, training_config.MODEL_NAME)
     # Only then train the model with the best hyperparameters
     return run_training(model_params, training_config.COMMIT_SHA, training_config.MODEL_NAME)
