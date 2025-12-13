12a6d997542b0dcdadd73e6a5d1884b38fe7be2d
vasco <vasco.dias23@outlook.com>
2025-12-13 03:13:26 +0000

    Env in 3-continuous-staging.yml fixed, trigger train

--- src/training_api/main.py
+++ src/training_api/main.py
@@ -15,9 +15,9 @@ logger = logging.getLogger(__name__)
 def train(training_config: TrainingConfig):
     mlflow.set_experiment(f"{training_config.EXP_NAME}_{training_config.COMMIT_SHA}")
 
-    # first get the best hyperparameters
+    # First get the best hyperparameters
     model_params = run_hyperparameter_tuning(training_config.COMMIT_SHA, training_config.MODEL_NAME)
-    # only then train the model with the best hyperparameters
+    # Only then train the model with the best hyperparameters
     return run_training(model_params, training_config.COMMIT_SHA, training_config.MODEL_NAME)
 
 
